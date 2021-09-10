df2faba369de0219007045d6b01eb52fb1640229
Sai Anirudh <b.anirudh10@gmail.com>
2021-09-11 03:17:09 +0530

    AAdded To component state management

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -1,6 +1,39 @@
-import classes from "./top.module.css";
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+
+import { modeActions } from '../redux/mode';
+
+import { TIME, WORDS, TIME_MODE, WORDS_MODE } from '../constants';
+import classes from './top.module.css';
 
 function Top() {
+  const dispatch = useDispatch();
+  const mode = useSelector((state) => state.mode);
+
+  const toggleType = function (e) {
+    dispatch(modeActions.toggleType(e.target.textContent));
+  };
+
+  const toggleMode = function (e) {
+    let selectedMode = TIME === e.target.textContent ? TIME_MODE : WORDS_MODE;
+    dispatch(modeActions.changeMode(selectedMode));
+  };
+
+  const toggleOption = function (e) {
+    dispatch(modeActions.changeOption(+e.target.textContent));
+  };
+
+  const options = mode.allOptions.map((option) => (
+    <button
+      key={option}
+      className={`${classes.button} ${
+        option === mode.currOption ? classes.selected : ''
+      }`}
+      onClick={toggleOption}
+    >
+      {option}
+    </button>
+  ));
   return (
     <div className={classes.top}>
       <div className={classes.logo}>
@@ -9,29 +42,58 @@ function Top() {
       </div>
       <div className={classes.menu}>
         <div
-          className={classes["icon-button"]}
+          className={classes['icon-button']}
           id="startTestButton"
           tabIndex="2"
         >
-          <div className={classes["menu-icon"]}>
+          <div className={classes['menu-icon']}>
             <i className="fas fa-keyboard"></i>
           </div>
         </div>
-        <div className={classes["icon-button"]} id="trainButton" tabIndex="3">
-          <div className={classes["menu-icon"]}>
+        <div className={classes['icon-button']} id="trainButton" tabIndex="3">
+          <div className={classes['menu-icon']}>
             <i className="fas fa-dumbbell"></i>
           </div>
         </div>
       </div>
       <div className={classes.config}>
         <div className={classes.group}>
-          <button className={classes.button}>punctuation</button>
-          <button className={classes.button}>numbers</button>
+          <button
+            className={`${classes.button} ${
+              mode.punctuation ? classes.selected : ''
+            }`}
+            onClick={toggleType}
+          >
+            punctuation
+          </button>
+          <button
+            className={`${classes.button} ${
+              mode.numbers ? classes.selected : ''
+            }`}
+            onClick={toggleType}
+          >
+            numbers
+          </button>
         </div>
         <div className={classes.group}>
-          <button className={classes.button}>time</button>
-          <button className={classes.button}>words</button>
+          <button
+            className={`${classes.button} ${
+              mode.currMode === TIME ? classes.selected : ''
+            }`}
+            onClick={toggleMode}
+          >
+            time
+          </button>
+          <button
+            className={`${classes.button} ${
+              mode.currMode === WORDS ? classes.selected : ''
+            }`}
+            onClick={toggleMode}
+          >
+            words
+          </button>
         </div>
+        <div className={classes.group}>{options}</div>
       </div>
     </div>
   );
